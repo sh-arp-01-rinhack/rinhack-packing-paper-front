9172bf2dc411cc823a2f491d22b3d2b65f056558
diewittol <slavigrits@gmail.com>
2024-11-29 20:08:12 +0300

    initial implimentation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { ErrorPageComponent } from './core/error-page/error-page.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/homepage', pathMatch: 'full' },
     { path: 'homepage', component: HomepageComponent },
+    { path: 'about', component: AboutComponent },
     { path: 'error', component: ErrorPageComponent },
 
     { path: '**', redirectTo: 'error' }
